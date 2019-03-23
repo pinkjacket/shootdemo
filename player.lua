d7a992cc72ca3df0635a63eeac990d59474f5107
James Donlan <jacketlizard@live.com>
2019-03-23 00:57:50 -0700

    player bound within game window

--- player.lua
+++ player.lua
@@ -5,6 +5,7 @@ function Player:new()
   self.x = 300
   self.y = 20
   self.speed = 500
+  self.width = self.image:getWidth()
 end
 
 function Player:update(dt)
@@ -12,6 +13,17 @@ function Player:update(dt)
     self.x = self.x - self.speed * dt
   elseif love.keyboard.isDown("right") then
     self.x = self.x + self.speed * dt
+  end
+  
+  -- window width
+  local window_width = love.graphics.getWidth()
+  
+  -- if x is too far left, set to 0
+  if self.x < 0 then
+    self.x = 0
+    -- if too far to the right, set to window width
+  elseif self.x + self.width > window_width then
+    self.x = window_width - self.width
   end
 end
 
