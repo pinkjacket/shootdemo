4bfa5db2d92bb6d0364e34b6acb78a761aa6b090
James Donlan <jacketlizard@live.com>
2019-03-23 01:51:59 -0700

    Enemy spawns, moves within screen

--- main.lua
+++ main.lua
@@ -1,14 +1,18 @@
 function love.load()
   Object = require "classic"
   require "player"
+  require "enemy"
   
   player = Player()
+  enemy = Enemy()
 end
 
 function love.update(dt)
   player:update(dt)
+  enemy:update(dt)
 end
 
 function love.draw()
   player:draw()
+  enemy:draw()
 end
